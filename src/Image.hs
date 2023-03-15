57f14d71f0c999af24d53759db52e13eb3d9ccbb
James Carlson <jxxcarlson@gmail.com>
2023-03-15 16:01:08 -0400

    Progress: I'm now getting the upload URL back from Cloudlfare

--- src/Image.hs
+++ src/Image.hs
@@ -46,15 +46,7 @@ instance FromJSON CFUploadData where
   parseJSON = withObject "CFUploadData" $ \o -> do
       id <- o .: "id"
       uploadURL <- o .: "uploadURL"
-      return $ CFUploadData id uploadURL 
-
-
--- instance FromJSON CFUploadData where
---   parseJSON (Object v) =
---     CFUploadData
---       <$> v .: "id"
---       <*> v .: "uploadURL"
---     return $ CFUploadData id uploadURL
+      return (CFUploadData id uploadURL) 
 
 
 instance FromJSON CFUploadResponse where
@@ -64,8 +56,6 @@ instance FromJSON CFUploadResponse where
             v .: "success"   <*>
             v .: "errors"    <*>
             v .: "messages"
-
-
 
 uploadResult :: CFUploadResponse -> CFUploadData
 uploadResult (CFUploadResponse result  _ _ _) = result
@@ -87,49 +77,10 @@ requestCFToken = do
             ]
             }
     response <- httpLbs request manager 
-    let cfUploadResponse = (Data.Aeson.decode $ responseBody response)  :: Maybe CFUploadData
+    let cfUploadResponse = (Data.Aeson.decode  (responseBody response))  :: Maybe CFUploadResponse
     case cfUploadResponse of 
       Nothing -> return $  show "Could not decode Cloudflare's response: " ++ (show $ responseBody response)
       Just daata -> return $ show daata
-
-
-jsonStr = "{\n  \"result\": {\n    \"id\": \"7bff08f3-9c52-4d2f-21d1-923fae172300\",\n    \"uploadURL\": \"https://upload.imagedelivery.net/9U-0Y4sEzXlO6BXzTnQnYQ/7bff08f3-9c52-4d2f-21d1-923fae172300\"\n  },\n  \"success\": true,\n  \"errors\": [],\n  \"messages\": []\n}"
-
--- {
---   "result": {
---     "id": "7bff08f3-9c52-4d2f-21d1-923fae172300",
---     "uploadURL": "https://upload.imagedelivery.net/9U-0Y4sEzXlO6BXzTnQnYQ/7bff08f3-9c52-4d2f-21d1-923fae172300"
---   },
---   "success": true,
---   "errors": [],
---   "messages": []
--- }
-
- 
-
--- requestCFToken :: IO (String)
--- requestCFToken = do
---     manager <- newTlsManager -- create a new manager
---     let url = "https://api.cloudflare.com/client/v4/accounts/" ++ cloudFlareAccountId ++ "/images/v2/direct_upload"
---         -- payload = object ["name" .= "Alice", "age" .= (25 :: Int)]
---         request = (parseRequest_ url)
---             { method = "POST"
---             -- , requestBody = RequestBodyLBS $ encode payload
---             , requestHeaders = [("Content-Type", "application/json"), 
---               ("Authorization", BL.toStrict $ B.toLazyByteString $ "Bearer " <> B.stringUtf8 cloudFlareAPIKey) 
---             ]
---             }
---     response <- httpLbs request manager -- send the request
---     let cfUploadResponse = (Data.Aeson.decode $ responseBody response)  :: Maybe CFUploadData
---     case cfUploadResponse of 
---       Nothing -> return $  show "Could not decode Cloudflare's response: " ++ (show $ responseBody response)
---       Just daata -> return $ show daata
-
-
-
-    -- putStrLn $ show $ responseBody response
-    -- document <- jsonData :: ActionM Document
-    -- return $ responseBody response
     
 
 cloudFlareAPIKey = "wmBxrHLMHFfBa4P5M6gGfS-w8dYJELRu74dA_wcy"
@@ -167,11 +118,3 @@ instance ToJSON CFImage where
 
 byteStringToText byteString = TL.unpack $ TLE.decodeUtf8 byteString
 
-
--- instance FromJSON CFUploadResponse where
---   parseJSON = withObject "Response" $ \o -> do
---     result <- o .: "result"
---     success <- o .: "success"
---     errors <- o .: "errors"
---     messages <- o .: "messages"
---     return $ CFUploadResponse result success errors messages
