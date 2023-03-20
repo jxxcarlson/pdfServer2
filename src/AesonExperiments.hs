d1409780009549076816d28c1bed36b8df539245
James Carlson <jxxcarlson@gmail.com>
2023-03-19 22:23:03 -0400

    Experimenting with nested decoders ... some success

---
+++ src/AesonExperiments.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE DeriveGeneric  #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+
+module AesonExperiments where
+
+import Data.Aeson
+import Data.Aeson.Types (parseMaybe, parseEither, Parser)
+import GHC.Generics
+import qualified Data.ByteString.Lazy as LB
+import Data.Text
+
+fromJSONValue :: FromJSON a => Value -> Maybe a
+fromJSONValue = parseMaybe parseJSON
+
+value :: Value
+value = object [ "first_name" .= ("Juniper" :: String), "last_name" .=( "Lerrad" :: String) ]
+
+
+data Foo = Foo
+  { field1 :: Int
+  , field2 :: String
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+  -- ToJSON so that we can encode *to* a JSON string,
+  -- FromJSON so that we can parse *from* a JSON string
+
+jsonString :: LB.ByteString
+jsonString = "{ \"field1\": 27, \"field2\": \"hello!\" }"
+
+maybeFoo :: Maybe Foo
+maybeFoo = decode jsonString
+
+-- { contact_info: { email: <string> } }
+nested :: Value -> Parser String
+nested = withObject "ContactInfo" $ \obj -> do
+  contact <- obj .: "contact_info"
+  contact .: "email"
+
+email = parseMaybe nested $ object
+    [ "contact_info" .=
+      object [ "email" .= ("williamyaoh@gmail.com" :: String) ]
+    ]  
+
+myFoo :: Foo
+myFoo = Foo
+  { field1 = 909
+  , field2 = "take your time"
+  }
+
+
+customValue :: Value
+customValue = object
+  [ "list_price" .= (150000 :: Int)
+  , "sale_price" .= (143000 :: Int)
+  , "description" .= ("2-bedroom townhouse" :: String)
+  ]
+
+data Person = Person
+  { firstName :: String
+  , lastName  :: String
+  }
+  deriving (Show)
+
+-- our fields are snake_case instead
+instance ToJSON Person where
+  toJSON (Person { firstName = firstName, lastName = lastName }) =
+    object [ "first_name" .= firstName
+           , "last_name"  .= lastName
+           ]
+
+instance FromJSON Person where
+  -- note that the typeclass function is parseJSON, not fromJSON
+  parseJSON = withObject "Person" $ \obj -> do
+    firstName <- obj .: "first_name"
+    lastName <- obj .: "last_name"
+    return (Person { firstName = firstName, lastName = lastName })
+
+karlJSON :: LB.ByteString
+karlJSON = "{\"first_name\":\"Karl\",\"last_name\":\"Popper\"}"
+
+(.->) :: FromJSON a => Parser Object -> Text -> Parser a
+(.->) parser key = do
+  obj <- parser
+  obj .: key
+
+nested' :: Value -> Parser (String, String)
+nested' = withObject "ContactInfo" $ \obj -> do
+   email <- obj .: "contact_info" .-> "email"
+   state <- obj .: "contact_info" .-> "address" .-> "state"
+   return (email, state)
--- src/CFUpload.hs
+++ src/CFUpload.hs
@@ -5,8 +5,11 @@ module CFUpload where
 
 
 import Data.Aeson
+import Data.Aeson.Types
 import GHC.Generics
 import Data.Text
+import qualified Data.ByteString.Lazy as LB (ByteString)
+
 import qualified Data.ByteString.Lazy.Char8 as BL
 
 data CFUploadResponse = CFUploadResponse
@@ -15,7 +18,7 @@ data CFUploadResponse = CFUploadResponse
     success  :: Bool,
     errors   :: [String],
     messages :: [String]
-  } deriving (Generic, Show)
+  } deriving (Show)
 
 data CFUploadResult = CFUploadResult {
     id                :: String,
@@ -23,11 +26,94 @@ data CFUploadResult = CFUploadResult {
     uploaded          :: String,
     requireSignedURLs :: Bool,
     variants          :: [String]
-  } deriving  (Generic, Show)
+  } deriving  (Show)
 
-instance FromJSON CFUploadResponse
+cfData = object [
+    "result" .= object  [ 
+        "id" .= ("abc" :: String)
+        , "filename" .= ("foo.jpg" :: String)
+        , "uploaded" .= ("2023-03-19" :: String)
+        , "requireSignedUrls" .= (False ::Bool)
+        ,  "variants" .= (["foobar@yada.io/bird.jpg"] :: [String])
+    ]
+    , "success" .= (True :: Bool)
+    , "errors" .= ([]:: [String])
+    , "messages" .= ([]::  [String])
+   ]
 
-instance FromJSON CFUploadResult
+
+foo = object
+    [ "contact_info" .= object
+      [ "email" .= ("williamyaoh@gmail.com" :: String)
+      , "address" .= object
+        [ "state" .= ("OK" :: String)
+        , "zip_code" .= ("74008" :: String)
+        ]
+      ]
+   ]
+
+
+email = parseMaybe nested $ object
+    [ "contact_info" .=
+      object [ "email" .= ("williamyaoh@gmail.com" :: String) ]
+    ]  
+
+
+
+(.->) :: FromJSON a => Parser Object -> Text -> Parser a
+(.->) parser key = do
+  obj <- parser
+  obj .: key
+
+
+--  GHCi > parseMaybe CFUpload.nested' foo
+-- Just ("williamyaoh@gmail.com","OK")  
+nested' :: Value -> Parser (String, String)
+nested' = withObject "ContactInfo" $ \obj -> do
+   email <- obj .: "contact_info" .-> "email"
+   state <- obj .: "contact_info" .-> "address" .-> "state"
+   return (email, state)
+
+-- { contact_info: { email: <string> } }
+nested :: Value -> Parser String
+nested = withObject "ContactInfo" $ \obj -> do
+  contact <- obj .: "contact_info"
+  contact .: "email"
+
+--  GHCi > parseMaybe variantsP cfData
+-- Just ["foobar@yada.io/bird.jpg"]
+variantsP :: Value -> Parser [String]
+variantsP = withObject "CFUploadResult" $ \obj -> do
+    variants <- obj .: "result" .-> "variants"
+    return variants
+    
+-- getVariantsP :: BL.ByteString -> Either String [String]
+
+-- getVariantsPP input = do
+--   object <- eitherDecode input
+--   variantsP object
+
+
+instance FromJSON CFUploadResponse where
+     parseJSON (Object v) =
+        CFUploadResponse     <$>
+            v .: "result"    <*> 
+            v .: "success"   <*>
+            v .: "errors"    <*>
+            v .: "messages"
+
+-- Tell Aeson how to convert a CFUploadResponse object to a JSON string.
+
+instance FromJSON CFUploadResult where
+    parseJSON = withObject "CFUploadResult" $ \o -> do
+      id <- o .: Data.Text.pack  "id"
+      filename <- o .: Data.Text.pack "filename"
+      uploaded <- o .:  Data.Text.pack "uploaded"
+      requireSignedURLs <- o .: Data.Text.pack "requireSignedURLs"
+      variants <- o .: Data.Text.pack  "variants"
+      return (CFUploadResult id filename uploaded requireSignedURLs variants) 
+      
+
 
 testResponse = "{\n  \"result\": {\n    \"id\": \"49660d63-a43f-4011-1a7a-ff6435305d00\",\n    \"filename\": \"bird2.jpg\",\n    \"uploaded\": \"2023-03-16T23:08:22.768Z\",\n    \"requireSignedURLs\": false,\n    \"variants\": [\n      \"https://imagedelivery.net/9U-0Y4sEzXlO6BXzTnQnYQ/49660d63-a43f-4011-1a7a-ff6435305d00/public\"\n    ]\n  },\n  \"success\": true,\n  \"errors\": [],\n  \"messages\": []\n}"
 
